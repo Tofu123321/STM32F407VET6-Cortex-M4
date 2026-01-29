5663eea3583ba7b515e7c2b9f2f979c6940fec87
Tofu123321 <namvan200603@gmail.com>
2026-01-29 15:14:31 +0700

    m

---
+++ 7.I2C/MDK-ARM/RTE/_I2C/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'I2C' 
+ * Target:  'I2C' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
